5683bd8423f0cb4e480c76497533fa9b112451bf
Tunc Emran <tuncemran@hotmail.com>
2021-04-22 11:11:39 +0300

    first commit

---
+++ auth/AuthConfig.ts
@@ -0,0 +1,11 @@
+export const AuthConfig = {
+  appId: 'dfafd2a8-e2c4-42e4-8561-5d90efb670b8',
+  appScopes: [
+    'openid',
+    'offline_access',
+    'profile',
+    'User.Read',
+    'MailboxSettings.Read',
+    'Calendars.ReadWrite'
+  ]
+};
